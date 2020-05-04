e66e31cd0c9cf4b328d38dad054a24c1887bec03
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-05-04 14:34:00 -0500

    update

--- js/circle.js
+++ js/circle.js
@@ -9,18 +9,28 @@
             // TODO: complete this method
             // hint: area = pi * radius^2
 
-            return; // TODO: return the proper value
+
+            return Math.PI * Math.pow(this.radius, 2); // TODO: return the proper value
         },
+
+
+
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
-
+            if(doRounding == true) {
+                Math.round(this.radius);
+            } else {
+                Math.ceil(this.radius);
+            }
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
 
             console.log("Area of a circle with radius: " + this.radius + ", is: ");
         }
     };
+
+    circle.getArea();
 
     // log info about the circle
     console.log("Raw circle information");
