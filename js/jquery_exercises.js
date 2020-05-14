b2950b151c76e900b5dfd35249b62d7d94573074
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-05-13 19:31:36 -0500

    adding exercises

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -3,43 +3,76 @@
 $(document).ready(function() {
     alert( 'The DOM has finished loading!' );
 
-    var heading = $('#ramoni').html();
-    alert(heading);
+    //start Jquery Exercise for changing events...
+    $('#ramoni').click(function() {
+        console.log('h1 with id "codeup" was clicked');
 
-    console.log(heading);
+    //on click changes background of h1 to limegreen need to add h1 tag
+    $('#ramoni').click(function() {
+        $(this).css('background-color', 'limegreen')
+    });
 
-//change classes
 
-    $('.codeup').css({
-        'border': '1px solid red',
-        'background-color': 'lightblue'
 
 
     });
+    //changes paragraph size to font-size 18 when double clicked need to add p tag
+    $('.all-para').dblclick(function(){
+        $(this).css('font-size', '18px')
+    });
 
-    //element selectors
-
-    $('p').css('background-color', 'blue');
-
-    $('li').css('font-size', '20px');
-
-    $('h1').css('font-style', 'italic');
-
-    var alertPara = $('p').html();
-    alert(alertPara);
-
-    var alertLi = $('li').html();
-    alert(alertLi);
-
-    //multiple elements
-    $('h1, p, li').css({
-        'font-size': '50px'
+    // On hover goes red, off hover black. set li tag to red
+    $('.li-test').hover(function() {
+        $(this).css('color', 'red')
 
 
-    })
+        //second part of function
+    }, (function() {
+        $(this).css('color', 'black')
+    }));
 
-    $('.test').css("background-color", 'purple');
-    $('#container').css('background-color', 'red');
+
+
+
+    //end of it below is previous exercises
+
+//     var heading = $('#ramoni').html();
+//     alert(heading);
+//
+//     console.log(heading);
+//
+// //change classes
+//
+//     $('.codeup').css({
+//         'border': '1px solid red',
+//         'background-color': 'lightblue'
+//
+//
+//     });
+//
+//     //element selectors
+//
+//     $('p').css('background-color', 'blue');
+//
+//     $('li').css('font-size', '20px');
+//
+//     $('h1').css('font-style', 'italic');
+//
+//     var alertPara = $('p').html();
+//     alert(alertPara);
+//
+//     var alertLi = $('li').html();
+//     alert(alertLi);
+//
+//     //multiple elements
+//     $('h1, p, li').css({
+//         'font-size': '50px'
+//
+//
+//     })
+//
+//     $('.test').css("background-color", 'purple');
+//     $('#container').css('background-color', 'red');
 
 });
 
